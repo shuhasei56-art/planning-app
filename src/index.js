13736c6dcfa8816c5457c4113d7421648e7b3e3f
shuhasei56-art <shuhasei56@gmail.com>
2026-02-23 22:56:55 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import { App } from './App.js';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+document.addEventListener('DOMContentLoaded', () => {
+    new App();
+});
